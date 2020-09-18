b9b0ee4089378e130bf2435ee4b47b68b004b93c
mjronlines <mjronline@qq.com>
2020-09-18 11:51:19 +0800

    pub

--- test/flutter_particle_bg_test.dart
+++ test/flutter_particle_bg_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_particle_bg/flutter_particle_bg.dart';
+// import 'package:flutter_particle_bg/flutter_particle_bg.dart';
 
 void main() {
-//  test('adds one to input values', () {
-//    final calculator = Calculator();
-//    expect(calculator.addOne(2), 3);
-//    expect(calculator.addOne(-7), -6);
-//    expect(calculator.addOne(0), 1);
-//    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-//  });
+  // test('adds one to input values', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  //   // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  // });
 }
